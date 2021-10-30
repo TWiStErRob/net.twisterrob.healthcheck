0d0ec53b0182621fcc279439e49e7e23b5e11a91
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-10-30 22:57:17 +0100

    Redirect driver logging to SLF4J

--- src/main/kotlin/net/twisterrob/web/test/Browser.kt
+++ src/main/kotlin/net/twisterrob/web/test/Browser.kt
@@ -4,8 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
 import org.openqa.selenium.chrome.ChromeOptions
+import org.slf4j.LoggerFactory
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
 import java.time.Duration
+import kotlin.concurrent.thread
 
 object Browser {
 
@@ -14,11 +19,15 @@ object Browser {
 	}
 
 	fun createDriver(): WebDriver {
-		val driver = ChromeDriver(ChromeOptions().apply {
+		val options = ChromeOptions().apply {
 			if (Options.headless) {
 				addArguments("--headless")
 			}
-		})
+		}
+		val service = ChromeDriverService.createServiceWithConfig(options).apply {
+			sendOutputTo(loggerStream(LoggerFactory.getLogger(ChromeDriver::class.java)::info))
+		}
+		val driver = ChromeDriver(service, options)
 		driver.manage().apply {
 			timeouts().implicitlyWait(Duration.ofSeconds(10))
 			if (Options.headless) {
@@ -29,5 +38,17 @@ object Browser {
 			window().maximize()
 		}
 		return driver
+	}
+
+	/**
+	 * https://stackoverflow.com/a/69782433/253468
+	 */
+	private fun loggerStream(outputLine: (line: String) -> Unit): PipedOutputStream {
+		val output = PipedOutputStream()
+		val input = PipedInputStream(output).bufferedReader()
+		thread(isDaemon = true) {
+			input.lineSequence().forEach(outputLine)
+		}
+		return output
 	}
 }
