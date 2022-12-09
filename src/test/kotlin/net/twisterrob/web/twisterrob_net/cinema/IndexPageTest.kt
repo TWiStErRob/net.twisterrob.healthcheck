83621f7ba761261d6707b3ecabece2f434210903
Róbert Papp <papp.robert.s@gmail.com>
2022-12-09 18:55:11 +0000

    Cinema updates (#73)

--- src/test/kotlin/net/twisterrob/web/twisterrob_net/cinema/IndexPageTest.kt
+++ src/test/kotlin/net/twisterrob/web/twisterrob_net/cinema/IndexPageTest.kt
@@ -1,11 +1,15 @@
 package net.twisterrob.web.twisterrob_net.cinema
 
+import net.twisterrob.selenium.createPage
+import net.twisterrob.web.test.SmokeTest
 import net.twisterrob.web.test.WebDriverExtension
 import net.twisterrob.web.test.openTest
 import net.twisterrob.web.test.tags.justOpen
+import net.twisterrob.web.test.tags.navigation
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Tags
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.extension.ExtendWith
 import org.openqa.selenium.WebDriver
@@ -16,4 +20,15 @@ class IndexPageTest {
 	@Order(0)
 	@Tags(Tag(justOpen))
 	@TestFactory fun openTest(driver: WebDriver) = openTest<IndexPage>(driver)
+
+	@SmokeTest
+	@Tags(Tag(navigation))
+	@Test fun `redirects and secures from unsecure url`(driver: WebDriver) {
+		@Suppress("HttpUrlsUsage")
+		driver.get("http://cinema.twisterrob.net")
+
+		val page: IndexPage = driver.createPage()
+
+		page.assertOpened()
+	}
 }
--- src/test/kotlin/net/twisterrob/web/twisterrob_net/www/IndexPageTest.kt
+++ src/test/kotlin/net/twisterrob/web/twisterrob_net/www/IndexPageTest.kt
@@ -3,26 +3,24 @@ package net.twisterrob.web.twisterrob_net.www
 import net.twisterrob.selenium.createPage
 import net.twisterrob.web.test.SmokeTest
 import net.twisterrob.web.test.WebDriverExtension
+import net.twisterrob.web.test.openTest
 import net.twisterrob.web.test.tags.justOpen
 import net.twisterrob.web.test.tags.navigation
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Tags
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.extension.ExtendWith
 import org.openqa.selenium.WebDriver
 
 @ExtendWith(WebDriverExtension::class)
 class IndexPageTest {
 
+	@Order(0)
 	@Tags(Tag(justOpen))
-	@Test fun `home page can start up`(driver: WebDriver) {
-		val page = IndexPage(driver)
-
-		page.open()
-
-		page.assertOpened()
-	}
+	@TestFactory fun openTest(driver: WebDriver) = openTest<IndexPage>(driver)
 
 	@Tags(Tag(navigation))
 	@Test fun `home page can launch about`(driver: WebDriver) {
