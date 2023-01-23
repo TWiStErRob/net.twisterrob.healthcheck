a2c62d69b2ae6aa0f32c3841d3a7c9c13b5279f0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-23 16:22:57 +0000

    Fix deprecation

--- src/main/kotlin/net/twisterrob/web/test/Browser.kt
+++ src/main/kotlin/net/twisterrob/web/test/Browser.kt
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeDriverService
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.chromium.ChromiumDriverLogLevel
 import java.time.Duration
 
 object Browser {
@@ -21,10 +22,15 @@ object Browser {
 				addArguments("--headless")
 			}
 		}
-		val service = ChromeDriverService.createServiceWithConfig(options).apply {
-			// Uses platform's encoding via Charset.defaultCharset().
-			sendOutputTo(IoBuilder.forLogger(ChromeDriver::class.java).buildOutputStream())
-		}
+		val service = ChromeDriverService.Builder()
+			.withLogLevel(ChromiumDriverLogLevel.WARNING)
+			.build()
+			.apply {
+				// Redirect driver's output and error streams to proper logger. It'll show as:
+				// > [Exec Stream Pumper] org.openqa.selenium.chrome.ChromeDriver(MultiOutputStream.java:40)
+				// Uses platform's encoding via Charset.defaultCharset().
+				sendOutputTo(IoBuilder.forLogger(ChromeDriver::class.java).buildOutputStream())
+			}
 		val driver = ChromeDriver(service, options)
 		driver.manage().apply {
 			timeouts().implicitlyWait(Duration.ofSeconds(10))
